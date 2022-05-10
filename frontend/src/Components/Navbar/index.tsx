1caa89c40d666d8365796a0aefa4d20f23af8458
Renan Barbosa <renan-dealmeida@hotmail.com>
2022-05-10 15:15:04 -0300

    Routes

--- frontend/src/Components/Navbar/index.tsx
+++ frontend/src/Components/Navbar/index.tsx
@@ -8,7 +8,7 @@ function Navbar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/devsuperior">
+                    <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
                             <p className="dsmovie-contact-link">/devsuperior</p>
