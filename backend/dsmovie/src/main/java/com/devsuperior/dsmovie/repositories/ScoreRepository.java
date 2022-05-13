09f9966dfcf995ae4bfb7da862b2ae7e3f0e884d
Renan Barbosa <renan-dealmeida@hotmail.com>
2022-05-13 11:37:34 -0300

    First homolog-fixed

--- backend/dsmovie/src/main/java/com/devsuperior/dsmovie/repositories/ScoreRepository.java
+++ backend/dsmovie/src/main/java/com/devsuperior/dsmovie/repositories/ScoreRepository.java
@@ -4,15 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmovie.entities.Score;
 import com.devsuperior.dsmovie.entities.ScorePK;
-<<<<<<< HEAD
+
+
 
 public interface ScoreRepository extends JpaRepository<Score, ScorePK> {
 
-=======
-import com.devsuperior.dsmovie.entities.User;
-
-public interface ScoreRepository extends JpaRepository<Score, ScorePK> {
-
-	User findByEmail(String email);
->>>>>>> 23f394f55ef3b3eb821b380d4ea5139048f5ca1a
 }
