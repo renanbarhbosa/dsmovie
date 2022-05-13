4ad0e25c33c3b95021157a1cf906e2a9e1ab0244
Renan Barbosa <renan-dealmeida@hotmail.com>
2022-05-13 17:18:34 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -31,11 +31,13 @@ function Listing() {
     }, [pageNumber]);
 
 
-
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
 
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
 
             <div className="container">
                 <div className="row">
@@ -45,17 +47,8 @@ function Listing() {
                         </div>
                     )
                     )}
-
-
-
-
-
                 </div>
-
             </div>
-
-
-
         </>
     );
 
